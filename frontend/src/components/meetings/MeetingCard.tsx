f0021829840269f72fd2271e46a3934203878485
Rodolfo Kirch Veiga <rodolfoksveiga@gmail.com>
2021-07-07 15:40:47 +0200

    edit meeting card

--- frontend/src/components/meetings/MeetingCard.tsx
+++ frontend/src/components/meetings/MeetingCard.tsx
@@ -26,12 +26,15 @@ export default function MeetingCard({
     return (
         <Card className="m-2 m-md-3 shadow" style={{ width: '18rem' }}>
             <Card.Header className="text-center">
-                {local} - {mode}
+                Meeting #{meetingId}, in {local}
             </Card.Header>
             <Card.Body className="text-center">
                 <Card.Title className="font-weight-bold" as="h2">
                     {team1} x {team2}
                 </Card.Title>
+                <Card.Subtitle className="py-2">
+                    Best of <b>{mode}</b>
+                </Card.Subtitle>
                 <Card.Text className="font-italic text-muted">
                     {dateTime}
                 </Card.Text>
